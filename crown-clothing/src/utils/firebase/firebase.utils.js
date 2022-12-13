ef668c3c7f9bbb4d6c0e3e327cdb8a657beb95ce
DCamarco <doccamarco@gmail.com>
2022-12-13 12:33:45 -0500

    First Commit. Stage: Just integrated firebase authentication and database storage

---
+++ crown-clothing/src/utils/firebase/firebase.utils.js
@@ -0,0 +1,57 @@
+import { initializeApp } from "firebase/app";
+import {
+	getAuth,
+	signInWithRedirect,
+	signInWithPopup,
+	GoogleAuthProvider,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyA3VOxarImd0GcJwljk5co90YgONh5bNE4",
+	authDomain: "crown-clothingdb-d028c.firebaseapp.com",
+	projectId: "crown-clothingdb-d028c",
+	storageBucket: "crown-clothingdb-d028c.appspot.com",
+	messagingSenderId: "778564001954",
+	appId: "1:778564001954:web:7d09e55a1598e7c239b7fb",
+};
+
+// Initialize Firebase
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+	prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+	const userDocRef = doc(db, "users", userAuth.uid);
+
+	console.log(userDocRef);
+
+	const userSnapshot = await getDoc(userDocRef);
+	console.log(userSnapshot);
+	console.log(userSnapshot.exists());
+
+	if (!userSnapshot.exists) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
+
+		try {
+			await setDoc(userDocRef, {
+				displayName,
+				email,
+				createdAt,
+			});
+		} catch (error) {
+			console.log("error creating the user", error.message);
+		}
+	}
+	return userDocRef;
+};
