d4ed2f597016966985bcecb151503c403e96ce45
DCamarco <doccamarco@gmail.com>
2022-12-15 15:12:22 -0500

    adding cart context and shopping icon as well as cart dropdown

--- crown-clothing/src/utils/firebase/firebase.utils.js
+++ crown-clothing/src/utils/firebase/firebase.utils.js
@@ -87,5 +87,3 @@ export const signOutUser = async () => await signOut(auth);
 
 export const onAuthStateChangedListener = (callback) =>
 	onAuthStateChanged(auth, callback);
-
-//just for the git :)
